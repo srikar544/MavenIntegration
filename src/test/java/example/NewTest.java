77845e841605d5dbdac7f2a1da66764bc5ab3e51
skoppala <shanmukha_koppala@optum.com>
2017-05-24 20:32:20 +0530

    Commit No 4

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -13,6 +13,7 @@ public class NewTest {
 		public void testEasy() {	
 			System.out.println("starting execution");
                         System.out.println("starting execution");
+                        System.out.println("starting execution");
 			driver.get("http://demo.guru99.com/selenium/guru99home/");  
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
