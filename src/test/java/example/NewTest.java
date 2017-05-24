9a25b056f0ed6f14c0e307d0f2b2f79c54a29a65
skoppala <shanmukha_koppala@optum.com>
2017-05-24 19:16:07 +0530

    Commit New Code

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -11,16 +11,19 @@ public class NewTest {
 	    private WebDriver driver;		
 		@Test				
 		public void testEasy() {	
+			System.out.println("starting execution");
 			driver.get("http://demo.guru99.com/selenium/guru99home/");  
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
 		}	
 		@BeforeTest
 		public void beforeTest() {	
+			System.out.println("Before FirefoxDriver");
 		    driver = new FirefoxDriver();  
 		}		
 		@AfterTest
 		public void afterTest() {
+			System.out.println("After FirefoxDriver");
 			driver.quit();			
 		}		
 }	
